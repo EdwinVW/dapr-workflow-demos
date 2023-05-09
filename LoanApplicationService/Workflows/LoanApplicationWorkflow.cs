36e565ab45aacee244fabceb149b642b5a7a732a
EdwinVW <edwinvw@gmail.com>
2023-05-09 11:15:30 +0200

    Update workflow

--- LoanApplicationService/Workflows/LoanApplicationWorkflow.cs
+++ LoanApplicationService/Workflows/LoanApplicationWorkflow.cs
@@ -4,10 +4,18 @@ using System.Threading.Tasks;
 using Dapr.Workflow;
 using LoanApplicationService.Activities;
 using LoanApplicationService.Models;
-using Microsoft.DurableTask;
 
 public class LoanApplicationWorkflow : Workflow<LoanApplication, ApplicationResult>
 {
+    private string? _applicantName;
+    private decimal _loanAmount;
+    private decimal _yearlyGrossSalary;
+    private bool _existingCustomer = false;
+    private decimal? _outstandingAmount;
+    private bool? _hasDefaulted;
+    private int? _riskClass;
+    private string[]? _riskClauses;
+
     public override async Task<ApplicationResult> RunAsync(WorkflowContext context, LoanApplication application)
     {
         var logger = LoggerFactory.Create(builder =>
@@ -17,32 +25,43 @@ public class LoanApplicationWorkflow : Workflow<LoanApplication, ApplicationResu
 
         try
         {
+            _applicantName = application.ApplicantName;
+            _loanAmount = application.LoanAmount;
+            _yearlyGrossSalary = application.YearlyGrossSalary;
+
             // Determine whether the applicant is already an existing customer with us
             var customerInfo = await context.CallActivityAsync<CustomerInfo>(
-                    nameof(DetermineExistingCustomerActivity),
-                    application.ApplicantName);
-            bool existingCustomer = (customerInfo != null);
+                nameof(DetermineExistingCustomerActivity),
+                _applicantName);
+            if (customerInfo != null)
+            {
+                _existingCustomer = true;
+                _outstandingAmount = customerInfo.OutstandingAmount;
+                _hasDefaulted = customerInfo.HasDefaulted;
+            }
 
             // If the customer is new, register a new prospect
             ProspectInfo prospectInfo;
-            if (!existingCustomer)
+            if (!_existingCustomer)
             {
                 prospectInfo = await context.CallActivityAsync<ProspectInfo>(
                     nameof(RegisterProspectActivity),
-                    application.ApplicantName);
+                    _applicantName);
             }
 
             // Determine the Risk profile of the loan
             var applicationInfo = new ApplicationInfo(
-                LoanAmount: application.LoanAmount,
-                YearlyGrossSalary: application.YearlyGrossSalary,
-                ExistingCustomer: existingCustomer,
-                OutstandingAmount: existingCustomer ? customerInfo!.OutstandingAmount : null,
-                HasDefaulted: existingCustomer ? customerInfo!.HasDefaulted : null
+                LoanAmount: _loanAmount,
+                YearlyGrossSalary: _yearlyGrossSalary,
+                ExistingCustomer: _existingCustomer,
+                OutstandingAmount: _outstandingAmount,
+                HasDefaulted: _hasDefaulted
             );
-            var riskProfile = await context.CallActivityAsync<ProspectInfo>(
+            var riskProfile = await context.CallActivityAsync<RiskProfile>(
                 nameof(DetermineRiskProfileActivity),
                 applicationInfo);
+            _riskClass = riskProfile.RiskClass;
+            _riskClauses = riskProfile.RiskClauses;
 
             // Contact the customer
             logger.LogInformation("Waiting for external event: CustomerContacted ...");
